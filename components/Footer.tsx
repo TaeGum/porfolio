de6662c25e5c6d5f1b4452b78ae4185518525d30
LTG <suk0510@naver.com>
2025-04-25 16:52:37 +0900

    푸터 버튼 변경
    버튼 수정중

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,12 +5,27 @@ export function Footer() {
     <footer className="max-w-6xl mx-auto py-8 px-6 flex flex-col md:flex-row items-center justify-between">
       <div className="flex items-center gap-2">
         <img src="/vector1.svg" className="w-12 h-12" alt="logo" />
-        <span className="font-handlee text-[36px] text-primary" style={{ WebkitTextStroke: "0.5px #0c0c0c" }}>taegum</span>
+        <span
+          className="font-handlee text-[36px] text-primary"
+          style={{ WebkitTextStroke: "0.5px #0c0c0c" }}
+        >
+          taegum
+        </span>
       </div>
       <div className="flex gap-8 mt-4 md:mt-0">
-        <span className="bg-primary text-white font-handlee text-[32px] rounded-md px-6 py-2 shadow-subtle" style={{ WebkitTextStroke: "0.5px #fff" }}>Twitter X</span>
-        <span className="bg-primary text-white font-handlee text-[32px] rounded-md px-6 py-2 shadow-subtle" style={{ WebkitTextStroke: "0.5px #fff" }}>LinkedIn</span>
-        <span className="bg-primary text-white font-handlee text-[32px] rounded-md px-6 py-2 shadow-subtle" style={{ WebkitTextStroke: "0.5px #fff" }}>Instagram</span>
+        <span
+          className="bg-primary text-white font-handlee text-[32px] rounded-md px-6 py-2 shadow-subtle"
+          style={{ WebkitTextStroke: "0.5px #fff" }}
+        >
+          E-mail
+        </span>
+        <span
+          className="bg-primary text-white font-handlee text-[32px] rounded-md px-6 py-2 shadow-subtle"
+          style={{ WebkitTextStroke: "0.5px #fff" }}
+        >
+          LinkedIn
+        </span>
+        {/* <span className="bg-primary text-white font-handlee text-[32px] rounded-md px-6 py-2 shadow-subtle" style={{ WebkitTextStroke: "0.5px #fff" }}>Instagram</span> */}
       </div>
       {/* <div className="text-primary font-inter text-2xl mt-4 md:mt-0">Copyright. Portfolio 2024</div> */}
     </footer>
