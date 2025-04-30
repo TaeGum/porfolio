1fbc3751b5db962ec67bdd1c3daa61d4cf2d49b1
LTG <suk0510@naver.com>
2025-04-30 11:27:47 +0900

    반응형 적용

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,26 +4,31 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="flex items-center gap-32 justify-end max-w-6xl mx-auto py-8 px-6 relative z-10">
+    // <header className="flex items-center gap-32 justify-end max-w-6xl mx-auto py-8 px-6 relative z-10">
+    <header className="max-w-6xl mx-auto py-8 px-6 flex items-center gap-[4.813rem] md:gap-10 justify-end relative z-10">
       <div className="flex items-center gap-2">
-        <img src="/vector0.svg" className="w-8 h-8" alt="logo" />
+        <img
+          src="/vector0.svg"
+          className="w-8 h-8 sm:w-10 sm:h-10"
+          alt="logo"
+        />
         <span
-          className="font-handlee text-[20px] text-primary"
+          className="font-handlee text-lg sm:text-2xl md:text-3xl tracking-tight text-primary"
           style={{ WebkitTextStroke: "0.5px #0c0c0c" }}
         >
           LeeTaeGum
         </span>
       </div>
-      <nav className="flex gap-8">
+      <nav className="flex gap-4 sm:gap-8">
         <Link
           href="#portfolio"
-          className="font-inter text-[24px] text-primary hover:text-accent transition"
+          className="font-inter text-base sm:text-lg text-primary hover:text-accent transition"
         >
           Portfolio
         </Link>
         <Link
           href="#"
-          className="font-inter text-[24px] text-primary hover:text-accent transition"
+          className="font-inter text-base sm:text-lg text-primary hover:text-accent transition"
         >
           Contact Me
         </Link>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,30 +4,30 @@ import { Button } from "@/components/ui/button";
 
 export function Hero() {
   return (
-    <section className="relative flex flex-col md:flex-row items-center max-w-6xl mx-auto py-24 px-6 gap-16">
-      <div className="flex-1 flex flex-col gap-6 z-10">
+    <section className="relative w-full flex flex-col md:flex-row items-center max-w-6xl mx-auto md:py-24 px-4 md:gap-16 gap-8">
+      <div className="flex-1 flex flex-col gap-6 z-10 w-full">
         <div className="flex flex-row justify-between mb-8">
-          <h1 className="font-inter text-[40px] font-semibold text-primary">
-            직관적 UI와
+          <h1 className="font-inter text-sm sm:text-[1.75rem] md:text-[3rem] font-semibold text-primary">
+            UI & 시스템 설계 효율화를
             <br />
-            효율적 시스템 설계에 관심이 많은
+            시각화 하고자 하는
             <br />
             8년차 프론트엔드 개발자
           </h1>
           <div className="flex flex-col justify-between">
-            <p className="font-inter text-[24px] text-primary/90 max-w-xl mb-7">
-              다양한 프로젝트와 협업 경험을 바탕으로
+            <p className="font-inter text-[0.7rem] sm:text-[1.5rem] md:text-[2rem] text-primary/90 max-w-xl md:mb-7 sm:mb-7 mb-4">
+              10여개의 프로젝트 및 협업 과정에서 습득
               <br />
-              새로운 기술을 빠르게 학습하고 실무에 적용하며,
+              신규 기술을 실무에 적용, 전체 업무 효율화
               <br />
-              데이터 기반 개발과 유연한 커뮤니케이션으로
+              이를 기반으로 데이터를 쌓아
               <br />
-              실제 사용자와 비즈니스 모두에 가치를 더합니다.
+              데이터 기반 커뮤니케이션을 강화합니다.
             </p>
             <Button
-              size="lg"
+              // size="sm"
               // className="bg-primary text-white font-inter font-semibold py-4 px-14 rounded-md shadow-strong hover:bg-primary/90 transition w-52"
-              className="w-52 px-0"
+              className="text-[0.7rem] w-24 py-2 md:text-2xl sm:text-2xl md:py-[18px] sm:py-[18px] sm:w-52 md:w-52 px-3 sm:px-0 md:px-0"
             >
               Contact me
             </Button>
